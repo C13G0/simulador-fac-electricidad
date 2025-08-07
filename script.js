d078d0b30cdca07ac52f813f1a24fb50ff0e2829
C13G0 <blindprogramming@gmail.com>
2025-08-07 16:46:51 -0500

    Se crea repo de ejercicio funciones factura electrica

---
+++ script.js
@@ -0,0 +1,22 @@
+function consumoMensualKilovatios(){
+    let cantidadKilovatios = Number (prompt("Ingrese la cantidad de kilovatios consumidos en el mes: "))
+    return cantidadKilovatios
+    console.log("La cantidad de kilovatios es: " + cantidadKilovatios)
+
+    
+}
+
+    consumoMensualKilovatios()
+
+
+
+function tarifaBaseKilovatio(){
+    let tarifaKilovatio = Number (prompt("ingrese la tarifa por kilovatio:"))
+    console.log("la tarifa de kilovatio es:" + tarifaKilovatio)
+    return tarifaKilovatio
+}
+tarifaBaseKilovatio()
+
+
+
+
